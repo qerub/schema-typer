3bb2a30d064e809fc242fee5f44dcec2aa19d3df
Allen Rohner <arohner@gmail.com>
2014-11-04 14:41:25 -0600

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arohner/schema-typer "0.2.9"
+(defproject arohner/schema-typer "0.2.10"
   :dependencies [[org.clojure/core.typed "0.2.53"]
                  [prismatic/schema "0.2.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
